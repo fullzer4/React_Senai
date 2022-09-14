9b843c452a75f2b3f3f72349cc7ea66867f3b110
Gabriel Pelizzaro Pereira <53914594+fullzer4@users.noreply.github.com>
2022-09-14 04:07:14 -0300

    att

---
+++ cria_banco.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+connection = sqlite3.connect('bancoChillplace.db')
+cursor = connection.cursor()
+
+criar_tabela = "CREATE TABLE IF NOT EXISTS hoteis (hotel_id text PRIMARY KEY,\
+    nome text, estrelas real, diaria real, cidade text)"
+
+criar_hotel = "INSERT INTO hoteis VALUES ('alpha', 'alpha Hotel', 4.3, 345.30, 'Rio de janeiro')"
+cursor.execute(criar_tabela)
+cursor.execute(criar_hotel)
+connection.commit()
+connection.close()
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+from math import degrees
+from pickle import FALSE
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///bancoChillplace.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
 
 api.add_resource(Hoteis, "/hoteis")
 api.add_resource(Hotel, "/hoteis/<string:hotel_id>")
 
 if __name__ == "__main__":
+    from sql_alchemy import banco
+    banco.init_app(app)
     app.run(debug=True)
 
 #http://127.0.0.1:5000/hoteis
