61ce0f69f7e0d194e39a368654e63dbab6067c24
Gabriel Pelizzaro Pereira <53914594+fullzer4@users.noreply.github.com>
2022-09-20 08:47:34 -0300

    att

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from pickle import TRUE
+from flask import Flask, jsonify 
 from flask_restful import Api
+from blacklist import BLACKLIST
 from resources.hotel import Hoteis, Hotel
-from resources.usuario import User, UserRegister, UserLogin
+from resources.usuario import User, UserRegister, UserLogin, UserLogout
 from flask_jwt_extended import JWTManager
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bancoChillplace.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
+app.config['JWT_BLACKLIST_ENABLED'] = TRUE
 api = Api(app)
 jwt = JWTManager(app)
 
@@ -15,11 +19,20 @@ jwt = JWTManager(app)
 def cria_banco():
     banco.create_all()
 
+@jwt.token_in_blocklist_loader
+def verificar_blacklist(self,token):
+    return token["jti"] in BLACKLIST
+
+@jwt.revoked_token_loader
+def token_de_acesso_invalidado(jwt_header, jwt_payload):
+    return jsonify({'message', 'You have been logged out.'}), 401
+
 api.add_resource(Hoteis, "/hoteis")
 api.add_resource(Hotel, "/hoteis/<string:hotel_id>")
 api.add_resource(User, "/usuarios/<int:user_id>")
 api.add_resource(UserRegister, "/cadastro")
 api.add_resource(UserLogin, "/login")
+api.add_resource(UserLogout, "/logout")
 
 if __name__ == "__main__":
     from sql_alchemy import banco
--- resources/usuario.py
+++ resources/usuario.py
@@ -1,7 +1,8 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
-from flask_jwt_extended import create_access_token, jwt_required
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt
 from werkzeug.security import safe_str_cmp
+from blacklist import BLACKLIST
 
 class User(Resource):
 
@@ -46,3 +47,10 @@ class UserLogin(Resource):
             token_de_acesso = create_access_token(identity=user.user_id)
             return {"access_token": token_de_acesso}, 200
         return {"message":"The username or password is incorret"}, 401
+
+class UserLogout(Resource):
+    @jwt_required()
+    def post(self):
+        jwt_id = get_jwt()['jti']
+        BLACKLIST.add(jwt_id)
+        return {"message":"Logged out"}, 200
