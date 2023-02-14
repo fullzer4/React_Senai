210fa2f523e921e7007f548ae5523bb2ce946369
fullzer4 <gabrielpelizzaro@gmail.com>
2023-02-14 11:10:44 -0300

    new

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-USER = os.environ.get("USER")
-PASSWORD = os.environ.get("PASSWORD")
-DATABASE = os.environ.get("DATABASE")
-JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
-PORT = "5432"
-HOST = "localhost"
